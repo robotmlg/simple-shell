96d4374c6d048c32bd03660ae83830f264691ef0
Matt Goldman <robotmlg@gmail.com>
2015-03-20 22:41:35 -0400

    Increase maximum line length to 512, as in the FAQ

--- shell.h
+++ shell.h
@@ -4,9 +4,11 @@
 #define PIPE_CHAR '|'
 #define PROMPT_STR "$"
 
-#define MAX_INPUT_LENGTH 256 //Seems like a good length
+#define MAX_INPUT_LENGTH 512 //as defined in FAQ
 #define MAX_INPUT_TOKENS 50  //As defined by project spec
 
 //parse the input line into tokens
 char *lntok(char *);
+
+//parse the input line into 
 #endif
